ad3d4f381a3696f156a2f7b0365c49b4986dd5bb
SimonPlaye <playesimon@gmail.com>
2024-12-18 12:13:23 +0100

    fix: 404 error

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  basePath: '',
+  assetPrefix: '',
 };
-
-module.exports = {
-  output: "export",
-  basePath: "/<repository-name>",
-};
+module.exports = nextConfig;
 
 export default nextConfig;
