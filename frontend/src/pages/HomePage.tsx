84a1975bcecb88e6f4fe0bd16eb677ed64197250
SimonPlaye <playesimon@gmail.com>
2025-06-08 16:45:49 +0200

    feat: NotreCabinet

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import { CabinetName } from "components/HomePage/CabinetName/CabinetName";
 import styles from "./HomePage.module.css";
+import { NotreCabinet } from "components/HomePage/NotreCabinet/NotreCabinet";
 
 export const HomePage = (): React.JSX.Element => {
   return (
     <div className={styles.growContainer}>
       <CabinetName />
+      <NotreCabinet />
     </div>
   );
 };
