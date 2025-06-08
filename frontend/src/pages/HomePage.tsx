d823f76104f9e49133eecf06a9cb446f7e54d9a2
SimonPlaye <playesimon@gmail.com>
2025-06-08 19:02:29 +0200

    feat: NousContacter

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,6 +2,8 @@ import { CabinetName } from "components/HomePage/CabinetName/CabinetName";
 import styles from "./HomePage.module.css";
 import { NotreCabinet } from "components/HomePage/NotreCabinet/NotreCabinet";
 import { Competences } from "components/HomePage/Competences/Competences";
+import { Collaborateurs } from "components/HomePage/Collaborateurs/Collaborateurs";
+import { NousContacter } from "components/HomePage/NousContacter/NousContacter";
 
 export const HomePage = (): React.JSX.Element => {
   return (
@@ -9,6 +11,8 @@ export const HomePage = (): React.JSX.Element => {
       <CabinetName />
       <NotreCabinet />
       <Competences />
+      <Collaborateurs />
+      <NousContacter />
     </div>
   );
 };
