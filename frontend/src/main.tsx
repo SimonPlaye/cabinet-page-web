71069c557a7160b9650ed44368e46e5e8bf65816
SimonPlaye <playesimon@gmail.com>
2025-06-09 13:47:56 +0200

    fix: wrong path in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import { MainPage } from "pages/MainPage";
+import { MainPage } from "./pages/MainPage";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
