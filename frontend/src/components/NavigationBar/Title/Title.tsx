21cf7621b6002eb8356676f2a675bf8289f212cf
SimonPlaye <playesimon@gmail.com>
2025-06-09 12:08:16 +0200

    refactor: removie - in title

--- frontend/src/components/NavigationBar/Title/Title.tsx
+++ frontend/src/components/NavigationBar/Title/Title.tsx
@@ -8,7 +8,7 @@ export const Title = (): React.JSX.Element => {
     <Link to="/" className={styles.container}>
       <h1 className={styles.title}>
         <span className={styles.block}>Cabinet d&apos;avocat</span>
-        <span className={styles.block}>QUINTARD-PLAYE - JUILLAN</span>
+        <span className={styles.block}>QUINTARD PLAYE - JUILLAN</span>
       </h1>
     </Link>
   );
